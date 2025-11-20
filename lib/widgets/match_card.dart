46da1f82ff57e003b08c916d34918abfa4ca2f87
Paolo <gremipaolo@gmail.com>
2025-11-20 15:59:43 +0100

    added fields images and test in match card

--- lib/widgets/match_card.dart
+++ lib/widgets/match_card.dart
@@ -29,7 +29,57 @@ class MatchCard extends StatelessWidget {
     );
     final formattedDate = DateFormat('dd/MM/yyyy HH:mm').format(roundedDate);
 
+//----------------------------------------------------------------------------------------------
+return Card(
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(20),
+  ),
+  elevation: 6,
+  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
 
+  child: ClipRRect(    // serve per applicare il borderRadius all'immagine
+    borderRadius: BorderRadius.circular(20),
+    child: Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/montanaso.jpg"),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            // ignore: deprecated_member_use
+            Colors.black.withOpacity(0.25),   // scurisce leggermente lo sfondo (opzionale)
+            BlendMode.darken,
+          ),
+        ),
+      ),
+
+      child: ListTile(
+        title: Text(
+          '${match.scoreA} - ${match.scoreB}',
+          style: const TextStyle(color: Colors.white),
+        ),
+        subtitle: Text(
+          '$formattedDate\nBianchi: $playersA\nColorati: $playersB',
+          style: const TextStyle(color: Colors.white70),
+        ),
+        isThreeLine: true,
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (_) => MatchDetailScreen(match: match)),
+        ),
+        trailing: IconButton(
+          icon: const Icon(Icons.delete, color: Colors.red),
+          onPressed: () async {
+            final data = Provider.of<DataService>(context, listen: false);
+            await data.deleteMatch(match.id);
+            (context as Element).markNeedsBuild();
+          },
+        ),
+      ),
+    ),
+  ),
+);
+//----------------------------------------------------------------------------------------------
+/*
     return Card(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20),   // angoli arrotondati
@@ -54,6 +104,6 @@ class MatchCard extends StatelessWidget {
           },
         ),
       ),
-    );
+    );*/
   }
 }
