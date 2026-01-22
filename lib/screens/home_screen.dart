754617a53cc65a2a02d6e219db451efc22a98627
Paolo <gremipaolo@gmail.com>
2026-01-22 17:57:05 +0100

    fixed minor issue and added background behind card player

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -73,10 +73,18 @@ class _HomeScreenState extends State<HomeScreen> {
                 return Stack(
                   alignment: Alignment.center,
                   children: [
+                    // 🔥 Immagine di sfondo
+                    Positioned.fill(
+                      child: Image.asset(
+                        "assets/images/backgroundStadium.png",
+                        fit: BoxFit
+                            .cover, // Puoi usare contain o fill a seconda dell’effetto
+                      ),
+                    ),
                     // Scudo grande
                     SizedBox(
-                      width: maxWidth * 0.9, // 90% della larghezza disponibile
-                      height: maxHeight * 0.95, // 95% dell'altezza disponibile
+                      width: maxWidth, // 90% della larghezza disponibile
+                      height: maxHeight, // 95% dell'altezza disponibile
                       child: Image.asset(
                         "assets/shields/GoldShield.png",
                         fit: BoxFit.contain,
@@ -86,13 +94,13 @@ class _HomeScreenState extends State<HomeScreen> {
                     // Avatar posizionato nella parte superiore dello scudo
                     Positioned(
                       top: maxHeight *
-                          -0.2, // leggermente sotto il bordo superiore dello scudo
+                          -0.3, // leggermente sotto il bordo superiore dello scudo
 
                       child: Align(
                         //alignment: Alignment(0.5, -1), // <-- più negativo = più in alto
                         child: Transform.scale(
                           scale:
-                              0.3, // <-- riduci proporzionalmente (1.0 = 100%)
+                              0.25, // <-- riduci proporzionalmente (1.0 = 100%)
                           child: Image.asset(
                             "assets/icons/jack-removebg.png",
                             //width: 138, // doppio del raggio per coprire tutto il cerchio
@@ -103,17 +111,16 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     ),
                     Positioned(
-                        top: maxHeight * 0.55, // leggermente sotto il bordo superiore dello scudo
+                        top: maxHeight *
+                            0.55, // leggermente sotto il bordo superiore dello scudo
                         child: Align(
-                          child: 
-                          FifaStats(
-                            pac: 89,
-                            sho: 74,
-                            pas: 78,
-                            dri: 81,
-                            def: 71,
-                            phy: 73))
-                        ),
+                            child: FifaStats(
+                                pac: 89,
+                                sho: 74,
+                                pas: 78,
+                                dri: 81,
+                                def: 71,
+                                phy: 73))),
                   ],
                 );
               },
@@ -121,43 +128,46 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           // Metà inferiore – statistiche
           Expanded(
-  flex: 3,
-  child: Container(
-    padding: const EdgeInsets.all(20),
-    decoration: BoxDecoration(
-      borderRadius: const BorderRadius.vertical(top: Radius.circular(25)),
-      boxShadow: [
-        BoxShadow(
-          color: Colors.black12,
-          spreadRadius: 2,
-          blurRadius: 8,
-        )
-      ],
-      image: const DecorationImage(
-        image: AssetImage("assets/images/backgroundcity.png"), // <-- tua immagine
-        fit: BoxFit.cover, // COPRE tutta l’area mantenendo proporzioni
-      ),
-    ),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          "INFO GENERICHE",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.white, // consigliato con background scuro
+            flex: 4,
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    spreadRadius: 2,
+                    blurRadius: 8,
+                  )
+                ],
+                image: const DecorationImage(
+                  image: AssetImage(
+                      "assets/images/backgroundcity.png"), // <-- tua immagine
+                  fit:
+                      BoxFit.cover, // COPRE tutta l’area mantenendo proporzioni
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "INFO GENERICHE",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white, // consigliato con background scuro
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  StatRow(label: "Data di Nascita", value: "10 Agosto 1991"),
+                  StatRow(label: "Ruolo", value: "Attaccante"),
+                  StatRow(label: "Piede Preferito", value: "Sinistro"),
+                  StatRow(label: "Nazionalità", value: "Italiana"),
+                  StatRow(label: "Squadra Preferita", value: "Juventus"),
+                  StatRow(label: "Numero di Maglia", value: "21"),
+                ],
+              ),
+            ),
           ),
-        ),
-        const SizedBox(height: 10),
-        StatRow(label: "Data di Nascita", value: "10 Agosto 1991"),
-        StatRow(label: "Ruolo", value: "Attaccante"),
-        StatRow(label: "Piede Preferito", value: "Sinistro"),
-        StatRow(label: "Nazionalità", value: "Italiana"),
-      ],
-    ),
-  ),
-),
         ],
       ),
     );
@@ -187,7 +197,6 @@ class StatRow extends StatelessWidget {
   }
 }
 
-//--------------
 class FifaStats extends StatelessWidget {
   final int pac;
   final int sho;
